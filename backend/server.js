ef3e8f503969b85290cd690abb10e51264014cc8
SumeetShelar <sumeetshelar2000@gmail.com>
2025-10-01 21:19:25 +0530

    solved render frontend issue

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 app.use(cors({
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist"))); 
 
   app.get("/*splat", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist" ,"index.html"))
+    res.sendFile(path.resolve(__dirname, "../frontend/dist" ,"index.html"))
   );
 }
 
